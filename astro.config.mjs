9c8e0d8f4f8eb1ba8084305764f8380c57c64dd3
Siarhei Hrabliuk <kulbarh@gmail.com>
2024-09-02 18:57:11 +0300

    Add vercel adapter and rest param route

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    isr: {
+      exclude: ["/excluded/[...rest]"]
+    }
+  })
+});
